aa4bde3f1e83d1861d11cc94311f55d3fc4aab2d
Ирина Марковщенкова <irinamrk02@mail.ru>
2020-10-15 16:53:15 +0300

    8

--- 8/8.cpp
+++ 8/8.cpp
@@ -9,25 +9,16 @@ using namespace std;
 
 int main()
 {
-	int a[10] = {};
+	int a[10]  {};
+	int length = 10;
 	srand(time(NULL));
 	for (int i = 0; i < 10; i++)
 	{
 		a[i] = rand() % 10;
 		cout << a[i] << " ";
 	}
-	cout << endl;
-	for (int i = 0; i < a.length - 1; ++i)
-	{
-		int j = i;
-		for (int k = i + 1; k < a.length; ++k)
-		{
-			if (a[10] > a[j])
-				j = k;
-		}
-		std::swap(a[10], a[j]);
-	}
-	for (int index = 0; index < a.length; ++index)
-		std::cout << a[index] << " ";
-	return 0;
+	cout << "\n";
+	sort(a, a + 10);
+	for (int i = 0; i < length; ++i)
+		cout << a[i] << " ";
 }
